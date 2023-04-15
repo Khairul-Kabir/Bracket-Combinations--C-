e50a3f0302c577c58ba4563803ad2d3871871c7f
Khairul Kabir <sshakil.kabir60@gmail.com>
2023-04-15 23:07:04 +0600

    Code Upload

---
+++ Bracket-Combinations-using-C-Sharp/Bracket-Combinations-using-C-Sharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MainClass {
+
+  public static int BracketCombinations(int num) {
+
+    // code goes here  
+    if(num==0)
+    {
+      return 1;
+    }
+
+    int count = 0;
+
+    for(int i=0;i<num;i++){
+      count+= BracketCombinations(i)*BracketCombinations(num-i-1);
+
+    }
+    return count;
+
+  }
+
+  static void Main() {  
+
+    // keep this function call here
+    Console.WriteLine(BracketCombinations(Convert.ToInt32(Console.ReadLine())));
+    
+  } 
+
+}
